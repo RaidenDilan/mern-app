4e5ba43310b6cfdd44cbe778034620a69d66efaf
RaidenDilan <raiden18@me.com>
2020-05-09 00:12:41 +0100

    Sending a Login Request

--- frontend/src/user/pages/Auth/Auth.js
+++ frontend/src/user/pages/Auth/Auth.js
@@ -30,10 +30,32 @@ const Auth = () => {
   const authSubmitHandler = async event => {
     event.preventDefault();
 
-    if (isLoginMode) {}
+    setIsLoading(true);
+
+    if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const resData = await response.json();
+
+        if (!response.ok) throw new Error(resData.message);
+
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setIsError(err.message || 'Something went wrong, please try again.');
+      }
+    }
     else {
       try {
-        setIsLoading(true);
         const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
