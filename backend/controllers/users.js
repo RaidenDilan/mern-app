2b60c695e076bc9e9ee0f0d200b3ff91923a1ff9
RaidenDilan <raiden18@me.com>
2020-05-11 23:17:07 +0100

    Logging Users In (with Hashed Passwords)

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -74,12 +74,24 @@ const login = async (req, res, next) => {
     const error = new HttpError('Logging up failed, please try again later.', 500);
     return next(error);
   }
-
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser ) {
     const error = new HttpError('Invalid credentials, could not log you in.', 401);
     return next(error);
   }
 
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError('Could not log you in, please check your credentials and try again.', 500);
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError('Invalid credentials, could not log you in.', 401);
+    return next(error);
+  }
+  
   res.json({ message: 'Logged In', user: existingUser.toObject({ getters: true }) });
 };
 
