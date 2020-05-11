846d96a2adbfe01fcdb2577352ee39146ca68f9b
RaidenDilan <raiden18@me.com>
2020-05-11 23:28:32 +0100

    Generating Tokens (JWT) on the Backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,7 @@ const HttpError = require('../models/http-error');
 const { validationResult } = require('express-validator');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -61,7 +62,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    ); // key => 'supersecret_dont_share' or it could be any string.
+  } catch (err) {
+    const error = new HttpError('Signing up failed, please try again later.', 500);
+    return next(error);
+  }
+
+  res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token }); // It's up to the developer and what they want to return.
 };
 
 const login = async (req, res, next) => {
@@ -74,6 +87,7 @@ const login = async (req, res, next) => {
     const error = new HttpError('Logging up failed, please try again later.', 500);
     return next(error);
   }
+
   if (!existingUser ) {
     const error = new HttpError('Invalid credentials, could not log you in.', 401);
     return next(error);
@@ -91,8 +105,20 @@ const login = async (req, res, next) => {
     const error = new HttpError('Invalid credentials, could not log you in.', 401);
     return next(error);
   }
-  
-  res.json({ message: 'Logged In', user: existingUser.toObject({ getters: true }) });
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError('Logging in failed, please try again later.', 500);
+    return next(error);
+  }
+
+  res.json({ userId: existingUser.id, email: existingUser.email, token: token });
 };
 
 module.exports = {
