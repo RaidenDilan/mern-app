08cc72c13ce5dfd6f464f2c0505897391ffe4b35
RaidenDilan <raiden18@me.com>
2020-08-15 23:24:06 +0100

    Fixed leaked aws credentials.

--- backend/models/http-error.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error {
-  constructor(message, erroCode) {
-    super(message); // Adds a 'message' property
-    this.code = erroCode; // Adds a 'code' property
-  }
-}
-
-module.exports = HttpError;
